385b284e235e1ce4b2a6b7f17d4274f10643bfd3
unperturbable-se <raedovais321@gmail.com>
2025-11-17 20:36:28 +0500

    generic clearscreen and redraw window implementation of platform movement

--- flappybird.asm
+++ flappybird.asm
@@ -6,7 +6,6 @@ windowheight equ 50
 .code
 ;--------------------------------------
 makeBackground proc
-lea esi,window
 mov eax,0 ;index
 mov ecx,windowheight ;for(int i=0;i<100;i++)
 OuterLoop:
@@ -22,7 +21,6 @@ ret
 makeBackground endp
 ;--------------------------------------
 displayBackground proc
-lea esi,window
 mov eax,0 ;index
 mov ecx,windowheight ;for(int i=0;i<100;i++)
 OuterLoop:
@@ -41,9 +39,47 @@ OuterLoop:
 ret
 displayBackground endp
 ;--------------------------------------
+movePlatform proc ;K-T%K-->K is width
+push ax
+mov bx,windowwidth
+mov dx,0
+div bx
+pop ax
+sub ax,dx
+movzx ebx,ax
+movePlatform endp
+;--------------------------------------
+addplatform proc ;parameter ebx(x coordinate)
+mov eax,0 ;index
+mov ecx,windowheight ;for(int i=0;i<100;i++)
+OuterLoop:
+     push ecx
+     mov ecx,windowwidth ;for(int j=0;j<100;j++)
+     innerLoop:
+          cmp ecx,ebx ;if x is between [ebx,ebx+5]
+          jnae donothing
+          mov edx,ebx
+          add edx,5
+          cmp ecx,edx; 
+          jnbe donothing
+          mov byte ptr [esi+eax],' '
+          donothing:
+              inc eax
+              loop innerLoop
+     pop ecx
+     loop OuterLoop
+ret
+addplatform endp
+;--------------------------------------
 main proc
+lea esi,window
 call makeBackground
+;mov ax,cx
+;call movePlatform
+mov ebx,30
+call addplatform
 call displayBackground
+;call clrscr
 exit
 main endp
 ;----------------------------------------
