72d39384964e12630fb54ce539bb61692900a9a1
unperturbable-se <raedovais321@gmail.com>
2025-11-18 20:28:12 +0500

    randomized platform holes

--- flappybird.asm
+++ flappybird.asm
@@ -1,9 +1,10 @@
 INCLUDE Irvine32.inc
 .data
-windowwidth equ 100
-windowheight equ 100
+windowwidth equ 50
+windowheight equ 50
 platformSpeed equ 10
 platformWidth equ 4
+holeheight    equ windowheight/10
 .code
 ;--------------------------------------
     displayBackground proc ;runs only once
@@ -58,7 +59,7 @@ call SetTextColor
 ret
 movePlatform endp
 ;--------------------------------------
-addplatform proc ;parameter dl(x coordinate)
+addplatform proc uses edx;parameter dl(x coordinate)
 mov  eax,green+(black*16)
 call SetTextColor
 push ecx
@@ -99,14 +100,58 @@ outerLoop:
 ret
 removeplatform endp
 ;--------------------------------------
+addHole proc uses edx ecx ;parameter: dl(x coordinate),bl(y coordinate)
+;range-> x(dl to dl+pwidth) y(bl to bl+hheight)
+mov dh,bl
+mov  eax,yellow+(black*16)
+call SetTextColor
+mov ecx,platformWidth
+    outerloop:
+    cmp dl,0
+    jle endloop
+    push ecx
+    mov dh,bl
+    mov  ecx,holeheight
+         innerloop:
+         call gotoxy
+         mov al,219
+         call writechar
+         inc dh
+         loop innerLoop
+    pop ecx
+    inc dl
+    cmp dl,windowwidth-1
+    jg endloop
+    jmp outerloop
+
+endloop:
+mov  eax,green+(black*16)
+call SetTextColor
+ret
+addHole endp
+;--------------------------------------
+randomizehole proc uses eax ;returns bl(windowheight*.25 to windowheight*.75)
+mov eax,windowheight
+mov ebx,windowheight
+shr eax,1 ;0.5
+shr ebx,2; 0.25
+call randomrange
+add eax,ebx ;rand(0 to 0.5)+0.25
+mov bl,al
+ret
+randomizehole endp
+;--------------------------------------
 main proc
 call displayBackground
 outerloop:
-    mov dl,windowwidth-platformWidth
+    call randomizehole
+    mov dl,windowwidth-platformWidth-1
     call addplatform
+    call addHole
     mov dl,windowwidth-platformWidth-1
     loop1:
           call moveplatform
+          call addHole
           cmp dl,0
           jge loop1
     call removeplatform
